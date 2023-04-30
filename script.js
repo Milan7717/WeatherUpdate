270637d01bef11592afeed7fcb74af3fd159717c
milan <milan1mishra2@gmail.com>
2023-04-30 20:12:15 +0545

    weather upadate of your city

---
+++ script.js
@@ -0,0 +1,52 @@
+
+const apiKey = "0e8fe93d00705914693eeeb20a55f796";
+
+const weatherDataEl = document.getElementById("weather-data");
+
+const cityInputEl = document.getElementById("city-input");
+
+const inputFormEl = document.getElementById("input-form");
+
+inputFormEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const cityValue = cityInputEl.value;
+    getWeatherData(cityValue);
+
+});
+
+async function getWeatherData(cityValue) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`);
+
+        if (!response.ok) {
+            throw new Error("Network reaponae was not ok")
+        }
+        const data = await response.json()
+
+        const temperature = Math.floor(data.main.temp);
+
+        const description = data.weather[0].description;
+
+        const icon = data.weather[0].icon;
+
+        const details = [
+            `Feels like : ${Math.floor(data.main.feels_like)}`,
+            `Humidity: ${data.main.humidity}%`,
+            `Wind Speed : ${data.wind.speed} m/s`
+        ]
+        weatherDataEl.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png">`;
+        weatherDataEl.querySelector(".temperature").textContent = `${temperature}°C`;
+        weatherDataEl.querySelector(".description").textContent = description;
+
+        weatherDataEl.querySelector("details").innerHTML = details.map((details) => `<div>${details}</div>`).join("");
+
+    } catch (error) {
+        weatherDataEl.querySelector(".icon").innerHTML = "";
+        weatherDataEl.querySelector(".temperature").textContent = "";
+        weatherDataEl.querySelector(".description").textContent = "An error happened,plasee try again later.";
+
+        weatherDataEl.querySelector("details").innerHTML = "";
+
+
+    }
+}
